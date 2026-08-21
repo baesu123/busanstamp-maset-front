5e2e33f125a2aa94f9348a624fd98976adbe6fd6
bsh <qotngus72@naver.com>
2026-08-21 11:42:58 +0900

    Zustand 인증 Store

--- src/stores/authStore.js
+++ src/stores/authStore.js
@@ -1,11 +1,18 @@
 import { create } from "zustand";
 
+const ACCESS_TOKEN_KEY = "accessToken";
+
 export const useAuthStore = create((set) => ({
   user: null,
-  accessToken: localStorage.getItem("accessToken"),
 
+  accessToken: localStorage.getItem(ACCESS_TOKEN_KEY),
+
+  // 최초 인증 확인이 끝났는지 여부
+  isInitialized: false,
+
+  /**   * 로그인 성공 처리   */
   setAuth: ({ user, accessToken }) => {
-    localStorage.setItem("accessToken", accessToken);
+    localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
 
     set({
       user,
@@ -13,12 +20,25 @@ export const useAuthStore = create((set) => ({
     });
   },
 
-  logout: () => {
-    localStorage.removeItem("accessToken");
+  /**   * 새로고침 후 사용자 정보 복원   */
+  setUser: (user) => {
+    set({ user });
+  },
+
+  /**   * 로그인 상태 초기화   */
+  clearAuth: () => {
+    localStorage.removeItem(ACCESS_TOKEN_KEY);
 
     set({
       user: null,
       accessToken: null,
+    });
+  },
+
+  /**   * 최초 인증 확인 완료   */
+  finishInitialization: () => {
+    set({
+      isInitialized: true,
     });
   },
 }));
