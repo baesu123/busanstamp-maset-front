8645e575fa6ace7b05ad31b1ead471fc72d465a5
bsh <qotngus72@naver.com>
2026-08-21 15:29:51 +0900

    main.jsx에 인증 초기화 적용

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,19 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import "./index.css";
-import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+import App from "./App";
+import AuthInitializer from "./components/auth/AuthInitializer";
+import "./index.css";
 
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 30 * 1000, //30초 동안 같은 요청을 하지않는다.
-      retry: 1, //실패 시 1번 더 시도
-      refetchOnWindowFocus: false, //화면을 다른데 다녀와도 자동 요청을 하지 않음
+      staleTime: 30 * 1000,
+      retry: 1,
+      refetchOnWindowFocus: false,
     },
   },
 });
@@ -19,7 +22,9 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <QueryClientProvider client={queryClient}>
-        <App />
+        <AuthInitializer>
+          <App />
+        </AuthInitializer>
       </QueryClientProvider>
     </BrowserRouter>
   </StrictMode>,
