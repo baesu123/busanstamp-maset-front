3b8f6dc1491b5949922560dec524e7eb3c89fbcb
bsh <qotngus72@naver.com>
2026-08-21 10:15:43 +0900

    QueryClient 지정

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,24 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 30 * 1000, //30초 동안 같은 요청을 하지않는다.
+      retry: 1, //실패 시 1번 더 시도
+      refetchOnWindowFocus: false, //화면을 다른데 다녀와도 자동 요청을 하지 않음
+    },
+  },
+});
 
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
-  </BrowserRouter>,
+  <StrictMode>
+    <BrowserRouter>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </BrowserRouter>
+  </StrictMode>,
 );
