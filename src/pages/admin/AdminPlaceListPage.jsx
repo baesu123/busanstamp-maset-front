4d0c5f3d3f0f06571a00d2577e3429ad7ea44cf8
bsh <qotngus72@naver.com>
2026-08-24 12:40:46 +0900

    카카오 검색 버튼 추가

--- src/pages/admin/AdminPlaceListPage.jsx
+++ src/pages/admin/AdminPlaceListPage.jsx
@@ -93,7 +93,31 @@ function AdminPlaceListPage() {
         buttonText="새 장소 등록"
         buttonLink="/admin/places/new"
       />
+      <div className="mb-6 flex justify-end gap-3">
+        <Link
+          to="/admin/kakao-search"
+          className="
+      rounded-xl bg-amber-400
+      px-5 py-3 font-semibold
+      text-slate-900
+      transition hover:bg-amber-300
+    "
+        >
+          카카오에서 장소 찾기
+        </Link>
 
+        <Link
+          to="/admin/places/new"
+          className="
+      rounded-xl bg-violet-600
+      px-5 py-3 font-semibold
+      text-white
+      hover:bg-violet-500
+    "
+        >
+          직접 등록
+        </Link>
+      </div>
       <SearchForm
         searchForm={searchForm}
         onChange={handleSearchChange}
